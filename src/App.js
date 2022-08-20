e0e4c794d1aebd5488b78f8db687c313a64de1cd
Krystian S <mr.krystian.stawarczyk@gmail.com>
2022-08-20 14:24:35 +0200

    6

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 			<nav>
 				<ul>
 					<li>
-						<Link to="/"> Home</Link>
+						<Link to="/Clone"> Home</Link>
 					</li>
 					<li>
 						<Link to="/marketplace"> marketplace</Link>
@@ -32,7 +32,7 @@ function App() {
 			</nav>
 
 			<Routes>
-				<Route exact path="/" element={<Home />} />
+				<Route path="/Clone" element={<Home />} />
 				<Route path="/marketplace" element={<Marketplace />} />
 				<Route path="/pricing" element={<Pricing />} />
 				<Route path="/discover" element={<Discover />} />
